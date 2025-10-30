a0cfbb8b87725e0d0b44abc6988f0f2cd35ab787
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-10-30 16:12:31 +0200

    our seventh commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  // eslint-disable-next-line no-undef
-  <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/i-loom-elegant-clothes-website/' : '/'}>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
 )
