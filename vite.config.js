46b1477d078415dcaacc3ff5b44b824efdcfde46
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-10-30 09:31:44 +0200

    our sixth commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "I-LOOM-Elegant-Clothes-Website"
+  base: "/I-LOOM-Elegant-Clothes-Website"
 });
